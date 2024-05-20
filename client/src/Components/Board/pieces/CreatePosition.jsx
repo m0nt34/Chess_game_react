93f9fbf7e7f7b5e851e3e8f477b1abb7153e9bb7
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-05-20 21:28:09 +0400

    movable pieces

--- client/src/Components/Board/pieces/CreatePosition.jsx
+++ client/src/Components/Board/pieces/CreatePosition.jsx
@@ -25,3 +25,13 @@ export const CreatePositions = () => {
 
   return position;
 };
+
+export const copyPosition = (position) => {
+  const newPosition = new Array(8).fill("").map((x) => new Array(8).fill(""));
+  for (let rank = 0; rank < 8; rank++) {
+    for (let file = 0; file < 8; file++) {
+      newPosition[rank][file] = position[rank][file];
+    }
+  }
+  return newPosition;
+};
--- client/src/Components/Board/pieces/Pieces.jsx
+++ client/src/Components/Board/pieces/Pieces.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from "./Pieces.module.css";
 import Piece from "./piece/Piece";
-import { CreatePositions } from "./CreatePosition";
+import { CreatePositions ,copyPosition} from "./CreatePosition";
 const Pieces = () => {
-  const [Positions,UsePositions] = useState(CreatePositions())
-  console.log(Positions)
-  const onDrop = e =>{
-    console.log(e.dataTransfer.getData('text'))
-  }
-  const onDragOver = e =>{
-    e.preventDefault()
+  const [Positions, UsePositions] = useState(CreatePositions());
+  const ref = useRef(null);
 
+  const calculateCoords = (e) =>{
+    const { width, left, top } = ref.current.getBoundingClientRect();
+    console.log(e.dataTransfer.getData("text"));
+    const size = width / 8;
+    const y = 7-Math.floor((e.clientX - left)/size)
+    const x = 7- Math.floor((e.clientY - top)/size)
+    return {x,y}
   }
+  const onDrop = (e) => {
+    const newPosition = copyPosition(Positions)
+    const {x,y} = calculateCoords(e)
+    const [p, rank, file] = e.dataTransfer.getData("text").split(",");
+    newPosition[rank][file] = ''
+    newPosition[x][y] = p
+    UsePositions(newPosition)
+  };
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className={style.pieces} onDrop={onDrop} onDragOver={onDragOver}>
+    <div
+      className={style.pieces}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+      ref={ref}
+    >
       {Positions.map((r, rank) => {
         return r.map((f, file) => {
           return Positions[rank][file] ? (
