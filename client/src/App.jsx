f7244141bae20fe8a8006b276f3b27a041f8dbea
m0nt34 <116570394+m0nt34@users.noreply.github.com>
2024-05-21 23:12:45 +0400

    context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
-import Board from "./Components/Board/Board";
+import React from 'react'
+import { AppContext } from './Contexts/Context' // Adjust the import path as needed
+import Board from './Components/Board/Board' // Assuming you have a Board component
 
 function App() {
   return (
-    <div className="game">
-      <Board />
-    </div>
+    <AppContext.Provider value={{}}>
+      <div className="game">
+        <Board />
+      </div>
+    </AppContext.Provider>
   );
 }
 
---
+++ client/src/Contexts/Context.jsx
@@ -0,0 +1,8 @@
+import React, { createContext, useContext } from 'react'
+
+const AppContext = createContext()
+const useAppContext = () => {
+  return useContext(AppContext)
+}
+
+export { AppContext, useAppContext }
